787b44376059c160f819ad758ac96b291b70c918
Mhayk Whandson <hi@mhayk.com>
2021-02-27 17:27:13 +0000

    typeORM has been configured.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
+import 'reflect-metadata';
 import express from 'express';
+import "./database"
 
 const app = express();
 
-app.listen(3333, () => {
+
+/**
+ * GET => Liest/Search
+ * POST => Save/Create
+ * Put => Update
+ * DELETE => Delete
+ * PATH => specific edition
+ */
+
+app.get("/users", (request, response) => {
+    return response.json({ message: "Hello World" })
+})
+
+app.post("/users", (request, response) => {
+    return response.json({ message: "Data has been saved successfully." })
+})
+
+app.listen(3334, () => {
     console.log("Server is running!")
 })
