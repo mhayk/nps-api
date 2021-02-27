b4041faae8e4fadd2877ab95a405284289a6cc4a
Mhayk Whandson <hi@mhayk.com>
2021-02-27 20:01:42 +0000

    A new endpoint to "/surveys" has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/routers.ts
+++ src/routers.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
+import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysControler = new SurveysController();
 
 router.post("/users", userController.create);
+router.post("/surveys", surveysControler.create);
 
 export { router };
