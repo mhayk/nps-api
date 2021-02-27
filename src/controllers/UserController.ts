139737be80d8043635d9f073322469630f696f60
Mhayk Whandson <hi@mhayk.com>
2021-02-27 19:10:04 +0000

    Method create has been implementd in User controller.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,21 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { User } from "../models/User";
 
 class UserController {
     async create(request: Request, response: Response) {
-        const body = request.body;
+        const { name, email } = request.body;
 
-        return response.send(body)
+        const usersRepository = getRepository(User);
+
+        const user = usersRepository.create({
+            name,
+            email
+        })
+
+        await usersRepository.save(user)
+
+        return response.send(user)
     }
 }
 
