11aef385a62b879de68a4e816449e5067ad39dc4
Mhayk Whandson <hi@mhayk.com>
2021-02-28 10:19:43 +0000

    A endpoint to sendmail has been created.
    Signed-off-by: Mhayk Whandson <hi@mhayk.com>

--- src/routers.ts
+++ src/routers.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { SendMailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
 
@@ -6,10 +7,13 @@ const router = Router();
 
 const userController = new UserController();
 const surveysControler = new SurveysController();
+const sendMailController = new SendMailController();
 
 router.post("/users", userController.create);
 
 router.post("/surveys", surveysControler.create);
 router.get("/surveys", surveysControler.show);
+
+router.post("/sendmail", sendMailController.execute);
 
 export { router };
